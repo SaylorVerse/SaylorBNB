a3a76976d9be3ab218734102d66585b1cc883511
Pelumi527 <fatolupelumi@gmail.com>
2021-12-09 11:48:21 +0100

    updated version

--- contracts/SaylorBnb.sol
+++ contracts/SaylorBnb.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
